ec2b61e7fd06e016059ec49a68fd6b28a2d9749a
Emmanuel Cobian Duarte <45674567+EmmanuelCobian@users.noreply.github.com>
2019-10-01 16:37:48 -0700

    Implemented linear_drive into opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/WannaSmashBro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/WannaSmashBro.java
@@ -17,23 +17,29 @@ public class WannaSmashBro extends LinearOpMode
     {
         double left;
         double right;
+        double linear;
 
         robot.init(hardwareMap);
         waitForStart();
 
         while (opModeIsActive())
         {
-            left = Range.clip(-gamepad1.left_stick_y, -1, 1);
-            right = Range.clip(-gamepad1.right_stick_y, -1, 1);
+            double drive = -gamepad1.left_stick_y;
+            double turn = gamepad1.right_stick_x;
+            left = Range.clip(drive + turn, -1.0, 1.0);
+            right = Range.clip(drive - turn, -1.0, 1.0);
+            linear = Range.clip(-gamepad2.left_stick_y, -1, 1);
 
             telemetry.addData("left", left);
             telemetry.addData("right", right);
+            telemetry.addData("linear tower", linear);
+            telemetry.addData("Current Position", robot.linear_drive.getCurrentPosition());
             telemetry.update();
 
             robot.leftDrive.setPower(left);
             robot.rightDrive.setPower(right);
-
-            sleep(25);
+            robot.linear_drive.setPower(linear);
+            sleep(10);
         }
 
     }
