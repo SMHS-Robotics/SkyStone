18f16376a56f87f4844a3100415c565a98149531
CouriersRyan <zhangryan09@gmail.com>
2019-10-08 15:59:49 -0700

    Updated Autonomous
    Updated Autonomous enum
    Added AutonomousSkystone

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/AutonomousSkystone.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.opmodes.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class AutonomousSkystone extends LinearOpMode
+{
+    @Override
+    public void runOpMode()
+    {
+        
+    }
+}
