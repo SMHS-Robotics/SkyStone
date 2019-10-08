e45d5d57a354d0576ccbcdfc78370b6fd12a7aab
CouriersRyan <zhangryan09@gmail.com>
2019-10-08 16:27:21 -0700

    Update AutonomousSkystone.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/AutonomousSkystone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/AutonomousSkystone.java
@@ -4,9 +4,22 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public class AutonomousSkystone extends LinearOpMode
 {
+    AutonomousState checkPos = AutonomousState.CHECK_POSITION;
+
     @Override
     public void runOpMode()
     {
-        
+        while(opModeIsActive()){
+            switch(checkPos){
+                case CHECK_POSITION:
+                    checkPosition();
+                    break;
+                case GET_SKYSTONE_BLUE:
+                    break;
+            }
+        }
+    }
+
+    public void checkPosition(){
     }
 }
